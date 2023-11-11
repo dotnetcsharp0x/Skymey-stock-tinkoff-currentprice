f0c72b64489172a655a99c37892cf620305dcb21
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-11 11:09:24 +0300

    Добавьте файлы проекта.

---
+++ Skymey-stock-tinkoff-currentprice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Skymey_stock_tinkoff_currentprice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
